06f9c4087d2b171294b085a8241f8f7eb2f5272a
hjunie <hyeonjun000515@gmail.com>
2026-02-11 21:15:57 +0900

    로그인 작업

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -9,38 +9,20 @@ function Login() {
         console.log("비밀번호:", pw);
     };
 
-    return (<>
-    
-    </>
-        <div>
-          <h2>로그인</h2>
-
-          <input
-            type="text"
-            placeholder="아이디"
-            value={id}
-            onchanger={(e) => setid(e.target.value)}
-          />
-
-          <button onClick={handleLogin}>
-            로그인
-          </button>
-        </div>
-          
-    );
-}
-
-    
-        <div className="login-container">
+    return (
+       <>
+                <div className="login-container">
             <div className="login-box">
                 <h2>Login</h2>
 
                 <input type="text" placeholder="아이디"/>
                 <input type="password" placeholder="비밀번호" />
 
-                <button>로그인</button>
+                <button className="login-btn">로그인</button>
             </div>
         </div>
-    
-
+      </>
+          
+    );
+}
 export default Login;
